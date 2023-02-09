89212bf36149944c6bb615e761bee091e99cd660
TheEricT <124880810+TheEricT@users.noreply.github.com>
2023-02-09 10:37:20 +0000

    Add files via upload

---
+++ Reading file example/reading files.py
@@ -0,0 +1,58 @@
+# My example code for work placement at Red River - ERIC TYERMAN
+# This program will write random letters to a file, amount specified by the user.
+# It will then take a 3 letter input from the user and tell if the user's combination has appeared in the array
+
+import random
+
+# Creating array of letters that can be written to the file
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+letterAmount = int(input("How many letters do you want to read from?: "))
+
+# Checking if the amount specified by user is valid
+while letterAmount < 0:
+    letterAmount = int(input("Please pick an amount above 0: "))
+
+word = input("Using 3 letters, what letters would you like to search for? Place one space between each letter: ")
+l = word.split(" ")
+
+# Checking if the amount specified by user is valid
+while len(l) < 3:
+    word = input("Please use 3 letters. Place one space between each letter: ")
+    l = word.split(" ")
+    
+print("Your 3 letters are:", l)
+
+file = open("Letters.txt", "w")
+
+# Writes letters to file, seperating them with a empty space
+for i in range(letterAmount):
+    file.write(alphabet[random.randint(0,25)]+" ")
+
+file.close()
+
+file = open("Letters.txt")
+
+# Splits the letters in the file into an array using the empty space as an indicator as to where to split into individual elements
+lines = file.readline()
+data = lines.split(" ")
+
+letter1Count = 0
+letter2Count = 0
+count = 0
+for i in range(len(data)):
+    # if a letter in the array matches with the first letter of the combination, the algorithm moves onto the next part
+    if data[i] == l[0]:
+        letter1Count += 1
+        if data[i+1] == l[1] and len(l) > 1:
+            letter2Count += 1
+            if data[i+2] == l[2] and len(l) > 2:
+                count += 1
+
+# Outputs how many times the word appears, the first letter appears, and the first and second letter next to each other appears
+print(word, "appeared", count, "times in the array")
+print(l[0], "appeared", letter1Count, "times in the array")
+print(l[0], "and", l[1], "appeared", letter2Count, "times in the array")
+print("The array is shown below", "\n")
+print(data)
+
+file.close()
